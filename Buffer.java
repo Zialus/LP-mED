3ba73e44e6b6b9585f9f17747fe6fd71552f7f46
Raul Ferreira <raulmendesferreira@gmail.com>
2015-04-09 16:14:20 +0100

    Constructores da classe Buffer criados.

--- Buffer.java
+++ Buffer.java
@@ -3,7 +3,32 @@ import java.util.LinkedList;
 
 public class Buffer {
 
-	LinkedList<String> LineList = new LinkedList<String>();
+	LinkedList<StringBuilder> LineList = new LinkedList<StringBuilder>();
 	Position cursor;
+	
+	//Construir um buffer vazio
+	public Buffer() {
+		StringBuilder vazia = new StringBuilder();
+		vazia.append("");
+		LineList.add(vazia);
+		cursor = new Position(0,0);
+	}
+	
+	//Contruir um buffer já com uma linha
+	public Buffer(StringBuilder linha) {
+		LineList.add(linha);
+		StringBuilder vazia = new StringBuilder();
+		vazia.append("");
+		LineList.add(vazia);
+		cursor = new Position(0,0);
+	}
+	
+	
+	
+	
+	
+	void movePrev() {
+	}
+	
 	
 }
