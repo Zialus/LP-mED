d9a3df609fd441212414290151043b7f0bcdc0a8
Raul Ferreira <raulmendesferreira@gmail.com>
2015-04-09 20:55:15 +0100

    Removido código redundante(afixar strings vazias). Implementação de alguns métodos relacionados com o cursor.

--- Buffer.java
+++ Buffer.java
@@ -4,31 +4,65 @@ import java.util.LinkedList;
 public class Buffer {
 
 	LinkedList<StringBuilder> LineList = new LinkedList<StringBuilder>();
-	Position cursor;
+	Position currentCursor;
 	
 	//Construir um buffer vazio
 	public Buffer() {
 		StringBuilder vazia = new StringBuilder();
-		vazia.append("");
 		LineList.add(vazia);
-		cursor = new Position(0,0);
+		currentCursor = new Position(0,0);
 	}
 	
 	//Contruir um buffer já com uma linha
 	public Buffer(StringBuilder linha) {
 		LineList.add(linha);
 		StringBuilder vazia = new StringBuilder();
-		vazia.append("");
 		LineList.add(vazia);
-		cursor = new Position(0,0);
+		currentCursor = new Position(0,0);
 	}
 	
+	boolean validPosition(Position newCursor){
+		
+		int x = newCursor.getX();
+		int y = newCursor.getY();
+		
+		return( x>=0 && x<LineList.size() && y >= 0 && y<= LineList.get(x).length() );
+	}
 	
-	
-	
+
+	void setCursor(Position newCursor){
+		
+		int x = newCursor.getX();
+		int y = newCursor.getY();
+		
+		if ( validPosition(newCursor)){
+			currentCursor.setX(x);
+			currentCursor.setY(y);
+		}
+		else{
+			System.out.println("Buffer.setCursor : Invalid Position" + "x :" + x + " y: " + y);
+		}
+	}
 	
 	void movePrev() {
+		
+		int x = currentCursor.getX();
+		int y = currentCursor.getY();
+		
+		if ( currentCursor.getY() > 0){ currentCursor.setY(y-1); }
+		else if( currentCursor.getX() > 0){ currentCursor = new Position(x-1, LineList.get(x-1).length()); }
+		else {
+			System.out.println("Already at the begining");
+		}
+		
+		
+		
+		//LineList.get(x).length()	
+	
 	}
 	
+	void moveNext(){
+		
+	}
 	
 }
