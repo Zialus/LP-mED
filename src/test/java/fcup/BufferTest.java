76fffc34a0a47f8613f3f89693a431f9ad1deaaa
Raul Ferreira <raulmendesferreira@gmail.com>
2017-10-22 03:33:03 +0100

    Updated JUnit.

--- src/test/java/fcup/BufferTest.java
+++ src/test/java/fcup/BufferTest.java
@@ -1,7 +1,8 @@
 package fcup;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BufferTest {
 
