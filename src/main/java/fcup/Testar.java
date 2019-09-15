000fff7c219771501fcd13b170ee6300e9cd1393
Raul Ferreira <raulmendesferreira@gmail.com>
2019-09-15 18:27:46 +0100

    Moved tests to the right place.

--- src/main/java/fcup/Testar.java
+++ src/main/java/fcup/Testar.java
@@ -2,7 +2,6 @@ package fcup;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
@@ -12,15 +11,6 @@ public class Testar {
 
         System.out.println("-----------Testar Geral-----------------");
         test_geral();
-
-        System.out.println("-----------Testar Move Prev-------------");
-        test_move_prev();
-
-        System.out.println("-----------Testar Move Next-------------");
-        test_move_next();
-
-        System.out.println("-----------Testar Delete Char-----------");
-        test_delete_char();
 
     }
 
@@ -64,108 +54,6 @@ public class Testar {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void test_move_prev() {
-        Buffer buff_teste = new Buffer("123456789abcdef");
-        Cursor cursor1 = new Cursor(0,2);
-        buff_teste.setCursor(cursor1);
-        buff_teste.insertChar('\n');
-
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-
-        Cursor cursor2 = new Cursor(1,1);
-        buff_teste.setCursor(cursor2);
-
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.movePrev();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.movePrev();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.movePrev();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.movePrev();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.movePrev();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.movePrev();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-    }
-
-    private static void test_move_next() {
-        Buffer buff_teste = new Buffer("123456789abcdef");
-
-        Cursor cursor1 = new Cursor(0,13);
-        buff_teste.setCursor(cursor1);
-        buff_teste.insertChar('\n');
-
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        Cursor cursor2 = new Cursor(0,12);
-        buff_teste.setCursor(cursor2);
-        buff_teste.moveNext();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.moveNext();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.moveNext();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.moveNext();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.moveNext();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.moveNext();
-        System.out.println("Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-    }
-
-    private static void test_delete_char() {
-        Buffer buff_teste = new Buffer("123456789abcdef");
-        Cursor cursor1 = new Cursor(0,11);
-        buff_teste.setCursor(cursor1);
-        buff_teste.insertChar('\n');
-
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-        System.out.println("---------------------");
-
-        Cursor cursor2 = new Cursor(1,4);
-        buff_teste.setCursor(cursor2);
-
-        buff_teste.deleteChar();
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-        System.out.println("------------Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.deleteChar();
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-        System.out.println("------------Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.deleteChar();
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-        System.out.println("------------Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.deleteChar();
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        System.out.println(buff_teste.getNthLine(2));
-        System.out.println("------------Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.deleteChar();
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        //System.out.println(buff_teste.getNthLine(2));
-        System.out.println("------------Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
-        buff_teste.deleteChar();
-        System.out.println(buff_teste.getNthLine(0));
-        System.out.println(buff_teste.getNthLine(1));
-        //System.out.println(buff_teste.getNthLine(2));
-        System.out.println("------------Position " + " l: " + buff_teste.getCursor().getL() + " c: " + buff_teste.getCursor().getC());
     }
 
 }
--- src/test/java/fcup/BufferTest.java
+++ src/test/java/fcup/BufferTest.java
@@ -83,4 +83,176 @@ public class BufferTest {
         });
     }
 
+    @Test
+    public void testMovePrev() {
+        Buffer b = new Buffer("123456789abcdef");
+
+        Cursor c1 = new Cursor(0, 2);
+        b.setCursor(c1);
+        b.insertChar('\n');
+
+        assertEquals("12", b.getNthLine(0).toString());
+        assertEquals("3456789abcdef", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+
+        Cursor c2 = new Cursor(1, 1);
+        b.setCursor(c2);
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(1, b.getCursor().getC());
+
+        b.movePrev();
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.movePrev();
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(2, b.getCursor().getC());
+
+        b.movePrev();
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(1, b.getCursor().getC());
+
+        b.movePrev();
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.movePrev();
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.movePrev();
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+    }
+
+    @Test
+    public void testMoveNext() {
+        Buffer b = new Buffer("123456789abcdef");
+
+        Cursor c1 = new Cursor(0, 13);
+        b.setCursor(c1);
+        b.insertChar('\n');
+
+        assertEquals("123456789abcd", b.getNthLine(0).toString());
+        assertEquals("ef", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        Cursor cursor2 = new Cursor(0, 12);
+        b.setCursor(cursor2);
+
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(12, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(13, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(1, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(2, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(2, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(2, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.moveNext();
+        assertEquals(2, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+    }
+
+    @Test
+    public void test_delete_char() {
+        Buffer b = new Buffer("123456789abcdef");
+        Cursor cursor1 = new Cursor(0, 11);
+        b.setCursor(cursor1);
+        b.insertChar('\n');
+
+        assertEquals("123456789ab", b.getNthLine(0).toString());
+        assertEquals("cdef", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        Cursor cursor2 = new Cursor(1, 4);
+        b.setCursor(cursor2);
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(4, b.getCursor().getC());
+
+
+        b.deleteChar();
+        assertEquals("123456789ab", b.getNthLine(0).toString());
+        assertEquals("cde", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(3, b.getCursor().getC());
+
+        b.deleteChar();
+        assertEquals("123456789ab", b.getNthLine(0).toString());
+        assertEquals("cd", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(2, b.getCursor().getC());
+
+        b.deleteChar();
+        assertEquals("123456789ab", b.getNthLine(0).toString());
+        assertEquals("c", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(1, b.getCursor().getC());
+
+        b.deleteChar();
+        assertEquals("123456789ab", b.getNthLine(0).toString());
+        assertEquals("", b.getNthLine(1).toString());
+        assertEquals("", b.getNthLine(2).toString());
+
+        assertEquals(1, b.getCursor().getL());
+        assertEquals(0, b.getCursor().getC());
+
+        b.deleteChar();
+        assertEquals("123456789ab", b.getNthLine(0).toString());
+        assertEquals("", b.getNthLine(1).toString());
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            StringBuilder nthLine = b.getNthLine(2);
+        });
+
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(11, b.getCursor().getC());
+
+        b.deleteChar();
+        assertEquals("123456789a", b.getNthLine(0).toString());
+        assertEquals("", b.getNthLine(1).toString());
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            StringBuilder nthLine = b.getNthLine(2);
+        });
+
+        assertEquals(0, b.getCursor().getL());
+        assertEquals(10, b.getCursor().getC());
+    }
+
 }
