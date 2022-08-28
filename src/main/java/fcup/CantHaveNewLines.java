16f135b7156e48a9e4f90432789666ea6f80f83e
Raul Ferreira <raulmendesferreira@gmail.com>
2022-08-28 17:26:03 +0100

    Enable all compiler warnings and fix warning

--- src/main/java/fcup/CantHaveNewLines.java
+++ src/main/java/fcup/CantHaveNewLines.java
@@ -1,6 +1,7 @@
 package fcup;
 
 public class CantHaveNewLines extends Error {
+    private static final long serialVersionUID = -8845795791229001141L;
 
     public CantHaveNewLines(String message) {
         super (message);
